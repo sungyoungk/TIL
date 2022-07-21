96e6d3fa66a38b1116069a7208341381b20f1789
Sungyoung <101084642+sungyoungk@users.noreply.github.com>
2022-07-22 00:29:49 +0900

    Add files via upload

---
+++ Spring/스프링 핵심원리(객체지향)/src/test/java/order/OrderTest.java
@@ -0,0 +1,44 @@
+package order;
+
+import hello.core.AppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OrderTest {
+
+//    MemberService memberService = new MemberServiceImpl();
+//    OrderService orderService = new OrderServiceImpl();
+
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+
+    }
+
+
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order orderA = orderService.createOrder(memberId, "itemA", 10000 );
+
+        Assertions.assertThat(orderA.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
